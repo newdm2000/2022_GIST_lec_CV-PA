a73cca079a3adc2b381aa41d5d299d80f3f6fc30
Dongmin Shin <newdm2000@gmail.com>
2022-10-17 12:42:14 +0900

    PA2 initial commit

---
+++ PA2/Step2/load_matlab_file_in_python_example.py
@@ -0,0 +1,31 @@
+import matlab.engine
+import numpy as np
+import time
+import cv2
+
+# https://kr.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html
+# Window/Ubuntu terminal에서 "{Matlab 설치 경로}/extern/engines/python"으로 이동 후,
+# python setup.py install 실행
+
+# https://mscipio.github.io/post/matlab-from-python/
+
+def main():
+
+    eng = matlab.engine.start_matlab()
+    eng.addpath(r'C:\app\SfM', nargout=0) # 'calibrated_fivepoint.m'가 위치한 경로
+    end = time.time()
+    for i in range(100):
+        a = np.random.rand(3,5).tolist()
+        a = matlab.double(a)
+        b = np.random.rand(3, 5).tolist()
+        b = matlab.double(b)
+        E = eng.calibrated_fivepoint(a, b)
+        print(np.asarray(E))
+    print(time.time()-end)
+
+    eng.quit()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ PA2/Step7/README.txt
@@ -0,0 +1,7 @@
+Camera Calibration Toolbox for Matlab
+----------------------
+Go to
+http://robots.stanford.edu/cs223b04/JeanYvesCalib/
+
+Run the main matlab calibration function calib_gui (or calib).
+
