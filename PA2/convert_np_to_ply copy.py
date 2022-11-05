8fa0e66013627ca0d9b85ce6f686235fc805129e
Dongmin Shin <newdm2000@gmail.com>
2022-11-05 22:41:51 +0900

    2view clear

---
+++ PA2/convert_np_to_ply copy.py
@@ -0,0 +1,9 @@
+import open3d as o3d
+import numpy as np
+import pandas as pd
+df = pd.read_csv('./result/2view_Custom/3D_Point_Clouds_Two_Views.csv', sep=',', header=None)
+point_cloud = np.array(np.asarray(df.values)[1:, 1:], dtype=np.float32)
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(point_cloud[:, 0:3])
+pcd.colors = o3d.utility.Vector3dVector(point_cloud[:, 3:6] / 255.0)
+o3d.io.write_point_cloud("./result/2view_Custom/3D_Point_Clouds_Two_Views.ply", pcd)
